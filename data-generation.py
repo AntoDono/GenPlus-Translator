d93fe2d34f2409025403e8b736c45efbbb5284d8
AntoDono <anthony20151128@gmail.com>
2024-09-17 03:47:18 +0000

    waiting to generate

--- data-generation.py
+++ data-generation.py
@@ -1,9 +1,11 @@
 import requests
-import tdqm
+from tqdm import tqdm
 import os 
 from dotenv import load_dotenv
 from typing import List
 import random
+from threading import Thread
+import time
 import json
 
 load_dotenv()
@@ -22,11 +24,19 @@ def vocab_used(vocab: List, sentence: str) -> List:
     return res
     
 
-def read_vocab(path: str) -> List:
+def read_json(path: str) -> any:
     try:
         with open(path, 'r') as r:
             data = json.load(r)
         return data
+    except Exception as e:
+        print(e)
+        return []
+    
+def write_json(path: str, data: any) -> None:
+    try:
+        with open(path, 'w') as f:
+            json.dump(data, f)
     except Exception as e:
         print(e)
         return []
@@ -39,8 +49,13 @@ def get_generate_prompt(vocabulary: List, topic: str = "anything", vocab_size: i
     for term in vocab:
         vocab_string += f"\t - {term.get('word')}: {term.get('definition')}\n"
 
-    return f"The following consists of terms and its definitions, they can be used as verbs, adjectives and nouns:\n{vocab_string}\
-    \nOutput one sentence about {topic} that incorporates ALL of the terms."
+    return f"""
+Provide one sentence about {topic}, then rewrite the sentence using only a few slang terms from {vocab_string}. 
+Do not provide definitions or explanations. Keep the slang version no longer than one sentence.
+Example:
+Original: I value my family deeply.
+Translated: Ain't nothing like my fam, they got my back.
+"""
 
 
 def generate(prompt: str) -> str | None:
@@ -51,27 +66,83 @@ def generate(prompt: str) -> str | None:
     })
     return res.json().get("response")
 
-def generate_worker(max_vocab: int, turns: int, vocabulary: List, topics: List, return_list: List, pbar = None) -> None:
+def extract_from_generate(raw: str) -> List[str]:
+    if "Original" not in raw or "Translated" not in raw:
+        return []
+    else:
+        res = raw.split("Translated: ")
+        res[0] = res[0].replace("Original: ", "")
+        
+        return [res[0].strip(), res[1].strip()]
+
+def generate_worker(max_vocab: int, vocabulary: List, turns: int, topics: List, return_list: List, pbar = None, en_to_slang = True) -> None:
     
     cnt = 0
     
     while(cnt < turns):
         
-        prompt = get_generate_prompt(vocabulary, random.choice(topics), 10)
+        prompt = get_generate_prompt(vocabulary, random.choice(topics), max_vocab)
         res = generate(prompt)
-        if res == None:
+        extracted = extract_from_generate(res)
+
+        if len(extracted) != 2:
             continue
         
-        words_used = vocab_used(vocabulary, res)
-        return_list.append({
-            "sentence": res,
-            "terms": words_used
-        })
+        words_used = vocab_used(vocabulary, extracted[1])
+
+        if len(words_used) == 0:
+            continue
+
+        cnt += 1
         
-        if pbar != None:
+        if en_to_slang:
+            return_list.append({
+                "original": extracted[0],
+                "translated": extracted[1],
+                "terms": words_used
+            })
+        else:
+            return_list.append({
+                "original": extracted[1],
+                "translated": extracted[0],
+                "terms": words_used
+            })
+        
+        
+        write_json("./generated.json", generated_data)
+    
+        if pbar:
             pbar.update(1)
 
 if __name__ == "__main__":
-    pbar = tdqm(total=3000)
+
+    total_datasets = 1
+    generated_data = []
+    thread_num = 1
+    threads = []
+    vocabulary = read_json("./vocabulary.json")
+    topics = read_json("./topics.json")
+
+    print(f"Generating a dataset of length {total_datasets}")
+    pbar = tqdm(total=total_datasets)
+    
+    for i in range(thread_num):
+        t = Thread(target=generate_worker, args=[
+            len(vocabulary),
+            vocabulary,
+            int(total_datasets/thread_num),
+            topics,
+            generated_data,
+            pbar,
+            True
+        ])
+        t.start()
+        threads.append(t)
+        
+    for t in threads:
+        t.join()
+        
+    print("Completed generation.")
+        
     
     
